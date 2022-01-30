7e1cb77abbf6d611e4c6b832e6145dbd2e26d171
Ohrannick <76492189+Ohrannick@users.noreply.github.com>
2022-01-30 19:11:57 +0300

    added class & render

--- dist/js/script.js
+++ dist/js/script.js
@@ -130,7 +130,7 @@ window.addEventListener("DOMContentLoaded", () => {
   hideTabContent();
   showTabContent(); // Timer
 
-  const deadLine = "2022-01-28";
+  const deadLine = "2022-01-31";
 
   const getTimingRemaining = endTime => {
     const t = Date.parse(endTime) - Date.parse(new Date()),
@@ -186,8 +186,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function openModal() {
     modal.classList.toggle("hide");
-    document.body.style.overflow = "hidden";
-    clearInterval(modalTimerId);
+    document.body.style.overflow = "hidden"; // clearInterval(modalTimerId);
   }
 
   function closeModal() {
@@ -208,8 +207,7 @@ window.addEventListener("DOMContentLoaded", () => {
     if (e.code === "Escape" && !modal.classList.contains("hide")) {
       closeModal();
     }
-  });
-  const modalTimerId = setTimeout(openModal, 3000);
+  }); // const modalTimerId = setTimeout(openModal, 3000);
 
   function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -218,7 +216,53 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  window.addEventListener("scroll", showModalByScroll);
+  window.addEventListener("scroll", showModalByScroll); // Create menu_item
+
+  class CreateMenuItem {
+    constructor(src, alt, subtitle, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.subtitle = subtitle;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.transfer = 75;
+      this.parent = document.querySelector(parentSelector);
+      this.changeUSARUB();
+    }
+
+    changeUSARUB() {
+      this.price *= this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+
+      if (this.classes.length === 0) {
+        this.classes = "menu__item";
+        element.classList.add(this.classes);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+      <img src=${this.src} alt=${this.alt}>
+      <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+      <div class="menu__item-descr">${this.descr}</div>
+      <div class="menu__item-divider"></div>
+      <div class="menu__item-price">
+        <div class="menu__item-cost">Цена:</div>
+        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+      </div>
+      `;
+      this.parent.append(element);
+    }
+
+  }
+
+  new CreateMenuItem("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 9, ".menu .container").render();
+  new CreateMenuItem("img/tabs/elite.jpg", "elite", "Меню “Премиум”", "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!", 14, ".menu .container").render();
+  new CreateMenuItem("img/tabs/post.jpg", "elite", 'Меню "Постное"', "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!", 21, ".menu .container").render();
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -38,7 +38,7 @@ window.addEventListener("DOMContentLoaded", () => {
   showTabContent();
 
   // Timer
-  const deadLine = "2022-01-28";
+  const deadLine = "2022-01-31";
 
   const getTimingRemaining = (endTime) => {
     const t = Date.parse(endTime) - Date.parse(new Date()),
@@ -115,7 +115,7 @@ window.addEventListener("DOMContentLoaded", () => {
   function openModal() {
     modal.classList.toggle("hide");
     document.body.style.overflow = "hidden";
-    clearInterval(modalTimerId);
+    // clearInterval(modalTimerId);
   }
 
   function closeModal() {
@@ -141,7 +141,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  const modalTimerId = setTimeout(openModal, 3000);
+  // const modalTimerId = setTimeout(openModal, 3000);
 
   function showModalByScroll() {
     if (
@@ -154,4 +154,73 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   window.addEventListener("scroll", showModalByScroll);
+
+  // Create menu_item
+
+  class CreateMenuItem {
+    constructor(src, alt, subtitle, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.subtitle = subtitle;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.transfer = 75;
+      this.parent = document.querySelector(parentSelector);
+      this.changeUSARUB();
+    }
+
+    changeUSARUB() {
+      this.price *= this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+
+      if (this.classes.length === 0) {
+        this.classes = "menu__item";
+        element.classList.add(this.classes);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+      element.innerHTML = `
+      <img src=${this.src} alt=${this.alt}>
+      <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+      <div class="menu__item-descr">${this.descr}</div>
+      <div class="menu__item-divider"></div>
+      <div class="menu__item-price">
+        <div class="menu__item-cost">Цена:</div>
+        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+      </div>
+      `;
+      this.parent.append(element);
+    }
+  }
+
+  new CreateMenuItem(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    ".menu .container"
+  ).render();
+
+  new CreateMenuItem(
+    "img/tabs/elite.jpg",
+    "elite",
+    "Меню “Премиум”",
+    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+    14,
+    ".menu .container"
+  ).render();
+
+  new CreateMenuItem(
+    "img/tabs/post.jpg",
+    "elite",
+    'Меню "Постное"',
+    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+    21,
+    ".menu .container"
+  ).render();
 });
